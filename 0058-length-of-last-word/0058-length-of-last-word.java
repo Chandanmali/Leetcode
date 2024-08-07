5028277bd64a301e2b182d24cd4b73f8d791915b
Chandan Mali <88288584+Chandanmali@users.noreply.github.com>
2024-08-08 04:49:47 +0530

    Time: 0 ms (100%), Space: 41.7 MB (28.48%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,19 +1,20 @@
 class Solution {
-    public int lengthOfLastWord(String s) {
-        int length = 0;
-		
+    public int lengthOfLastWord(String s) 
+    {
+        int count = 0;
         String str = s.trim();
 
-		// We are looking for the last word so let's go backward
-        for (int i = str.length() - 1; i >= 0; i--) {
-            if (str.charAt(i) == ' ') { // a letter is found so count
+        for(int i = str.length()-1; i >= 0; i--)
+        {
+            if(str.charAt(i) != ' ')
+            {
+                count++;
+            }
+            else
+            {
                 break;
-            } else {  // it's a white space instead
-				//  Did we already started to count a word ? Yes so we found the last word
-                length++;
             }
         }
-
-        return length;
+        return count;
     }
 }
